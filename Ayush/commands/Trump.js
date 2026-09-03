76f471817d2de3c3ba011bd4d01c6e0617b6d352
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:10:26 +0500

    Update Trump.js

--- Ayush/commands/Trump.js
+++ Ayush/commands/Trump.js
@@ -4,7 +4,7 @@ module.exports.config = {
 
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Emma Olivia",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "trump [text]",
