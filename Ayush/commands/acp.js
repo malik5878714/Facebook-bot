a596b441243d620b6c0d1ebd7fbe5f3c58dcdff4
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:12:35 +0500

    Update acp.js

--- Ayush/commands/acp.js
+++ Ayush/commands/acp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Emma Olivia",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
   usages: "uid",
