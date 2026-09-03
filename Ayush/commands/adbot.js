b6001425f70937b372f77d9b40f5178956e4471c
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:13:44 +0500

    Update adbot.js

--- Ayush/commands/adbot.js
+++ Ayush/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "Emma Olivia",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
