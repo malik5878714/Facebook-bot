255b12e02272b8a50edabd8c6c045137e2112bbb
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:15:46 +0500

    Update adden.js

--- Ayush/commands/adden.js
+++ Ayush/commands/adden.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "anhdaden",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Emma Olivia",
   description: "White brother :v",
   commandCategory: "Edit-IMG",
   usages: "[text 1] | [text 2]",
