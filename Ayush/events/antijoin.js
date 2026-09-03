677cdd6491bb5cbc9380cd7c31d1e77596c1ca59
malik5878714 <malik5878714@gmail.com>
2026-09-03 21:55:21 +0500

    Update antijoin.js

--- Ayush/events/antijoin.js
+++ Ayush/events/antijoin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antijoin",
  eventType: ["log:subscribe"],
  version: "1.0.0",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "Emma Olivia",
  description: "Welcome new members to the group"
 };
 
