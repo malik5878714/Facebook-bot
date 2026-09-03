e47b9cb44692546eae5cbea8768b1e64edb83932
malik5878714 <malik5878714@gmail.com>
2026-09-03 21:56:08 +0500

    Update credits in anti-robbery script

--- Ayush/events/antirobbery.js
+++ Ayush/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "Emma Olivia",
     description: "Prevent admin changes",
 };
 
