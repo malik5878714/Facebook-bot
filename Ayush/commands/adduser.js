70e677df867d6bd26a88d680d68a4813953fbfb8
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:16:33 +0500

    Update adduser.js

--- Ayush/commands/adduser.js
+++ Ayush/commands/adduser.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adduser",
 	version: "2.4.3",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Emma Olivia",
 	description: "Add user to the group by link or id",
 	commandCategory: "group",
 	usages: "[args]",
