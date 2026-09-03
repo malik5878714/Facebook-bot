fe4010d2fc7381115092c764c8f05055214cb762
malik5878714 <malik5878714@gmail.com>
2026-09-03 22:07:29 +0500

    Update Obama.js

--- Ayush/commands/Obama.js
+++ Ayush/commands/Obama.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "obama",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Emma Olivia",
 	description: "Obama Tweet post",
 	commandCategory: "edit-img",
 	usages: "[text]",
