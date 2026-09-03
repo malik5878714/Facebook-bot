8205a71be7b4a1bd4753da20b63ca79a80f6f50a
malik5878714 <malik5878714@gmail.com>
2026-09-03 21:59:43 +0500

    Update god.js

--- Ayush/events/god.js
+++ Ayush/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
 	version: "1.0.0",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "Emma Olivia",
 	description: "Record bot activity notifications!",
     envConfig: {
         enable: true
